cf52cf2a2b77b198929ffc7f396ba6c8d03a4af2
hushun1214 <2375353463@qq.com>
2020-08-25 23:54:59 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -17,12 +18,27 @@ module.exports = {
       title: "Webpack-Demo",
       template: "./src/assets/index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+      ignoreOrder: false,
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        // use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development",
+            },
+          },
+          "css-loader",
+        ],
       },
     ],
   },
