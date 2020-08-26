4b3d5ce2b99505fa2cc79e7b47fa74b8657ec3fa
hushun1214 <2375353463@qq.com>
2020-08-26 09:15:18 +0800

    update

---
+++ webpack.config.base.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack-Demo",
+      template: "./src/assets/index.html",
+    }),
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const base = require("./webpack.config.base.js");
 module.exports = {
+  ...base,
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
   },
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack-Demo",
-      template: "./src/assets/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-      chunkFilename: "[id].[contenthash].css",
-      ignoreOrder: false,
-    }),
-  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        // use: ["style-loader", "css-loader"],
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "../",
-              hmr: process.env.NODE_ENV === "development",
-            },
-          },
-          "css-loader",
-        ],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
